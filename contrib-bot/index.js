d83ab08bd4fb1144839ae1c1e8af947b6b93434a
Merk Kremont <kremontmerk@gmail.com>
2022-01-06 02:53:38 +0700

    Bug fixing push commit

--- contrib-bot/index.js
+++ contrib-bot/index.js
@@ -1,6 +1,7 @@
 const jsonfile = require('jsonfile')
 const moment = require('moment')
 const random = require('random')
+const axios = require('axios')
 const git = require('simple-git')()
 
 require('dotenv').config()
@@ -10,7 +11,12 @@ const initialiseRepo = (git) => {
         .then(() => git.addRemote('origin', process.env.REPO_URL))
 }
 
-const makeCommit = (n) => {
+let doneCommitAmount = 0
+
+const makeCommit = async (git, n) => {
+    if (n === 0) {
+        return 
+    }
     const x = random.int(0, 54)
     const y = random.int(0, 6)
     const DATE = moment()
@@ -18,16 +24,53 @@ const makeCommit = (n) => {
         .add(1, "d")
         .add(x, "w")
         .add(y, "d")
-        .format();
+        .format()
     const data = {
         date: DATE,
-    };
+    }
 
     jsonfile.writeFile(process.env.DATA_PATH, data, () => {
         git
             .add([process.env.DATA_PATH])
-            .commit(DATE, { '--date': DATE }, makeCommit.bind(this, --n))
-            .push(['-u', 'origin', process.env.BRANCH_NAME], () => console.log('Processing => ', DATE, 'DONE'))
+            .commit(DATE, { '--date': DATE }, makeCommit.bind(this, git, --n))
+            .push(['-u', 'origin', process.env.BRANCH_NAME], () => {
+                console.log('Processing => ', DATE, 'DONE')
+                doneCommitAmount++
+                if (doneCommitAmount >= process.env.LOAD_COMMIT) {
+                    loadCurrentCommitEligibility()
+                }
+            })
+    })
+}
+
+const loadCurrentCommitEligibility = () => {
+    axios.get('https://route-api.dodoex.io/dodoapi/getdodoroute', {
+        params: {
+            fromTokenAddress: `0x794c979ea09Be49624C87E0ae9F0E0c3753a3e6a`,
+            fromTokenDecimals: `18`,
+            toTokenAddress: `0x4988a896b1227218e4A686fdE5EabdcAbd91571f`,
+            toTokenDecimals: `6`,
+            fromAmount: doneCommitAmount+`0000000000000000000000`,
+            slippage: `3`,
+            userAddr: `0x637Ab14AeC49F0cC54c735fB82bC3BFF5390d50E`,
+            chainId: `1313161554`,
+        }
+    })
+    .then(function (response) {
+        if (response.data.status==200) {
+            data=response.data.data
+            console.log(`\n\nCONGRATULATIONS!\n`)
+            console.log(process.env.GIT_EMAIL, `(`+process.env.GIT_NAME+`)`)
+            console.log(`You get:`, parseInt(doneCommitAmount+`0000`), `OSS`)
+            console.log(`Price (OSS/USDT):`, data.resAmount, `USDT`)
+            console.log(`Total Commits:`, doneCommitAmount)
+        } else {
+            console.log(`Something wen't wrong:`, response.data.data)
+        }
+        
+    })
+    .catch(function (error) {
+        console.log(`Something wen't wrong:`, error)
     })
 }
 
@@ -38,4 +81,4 @@ git
     .then(isRepo => !isRepo && initialiseRepo(git))
     .then(() => git.fetch())
 
-makeCommit(process.env.LOAD_COMMIT)
+makeCommit(git, process.env.LOAD_COMMIT)
